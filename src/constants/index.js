e7aaad35510fd41b0d369c52052127ac323b561f
diditaditya <di2t.aditya@gmail.com>
2017-06-15 09:39:08 +0700

    BACK UP - localhost

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,1 +1,2 @@
-export const url = 'http://hoax-fighter-backend-dev.ap-southeast-1.elasticbeanstalk.com/';
+// export const url = 'http://hoax-fighter-backend-dev.ap-southeast-1.elasticbeanstalk.com/';
+export const url = 'http://localhost:3002/';
